4afbfc36d48d93d405ab28e5ed20144735df0273
Shiva Krishnan <shivainfotech12@gmail.com>
2018-12-24 13:40:45 +0530

    Initial Project Generated By Spring Boot

---
+++ src/main/java/com/learning/petclinic/ShivasPetClinicApplication.java
@@ -0,0 +1,14 @@
+package com.learning.petclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShivasPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShivasPetClinicApplication.class, args);
+	}
+
+}
+
