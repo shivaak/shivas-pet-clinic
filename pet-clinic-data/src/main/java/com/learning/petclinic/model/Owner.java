3c857b10cb3534ed65c0d58c5297a5d0a212f0d6
Shiva Krishnan <shivainfotech12@gmail.com>
2019-01-08 22:52:05 +0530

    added contact info to owner

--- pet-clinic-data/src/main/java/com/learning/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/learning/petclinic/model/Owner.java
@@ -12,8 +12,14 @@ import java.util.Set;
 public class Owner extends Person{
 
 	private static final long serialVersionUID = -3202557158487199299L;
-	
+
 	private Set<Pet> pets;
+
+	private String address;
+
+	private String city;
+
+	private String phone;
 
 	public Set<Pet> getPets() {
 		return pets;
@@ -22,7 +28,33 @@ public class Owner extends Person{
 	public void setPets(Set<Pet> pets) {
 		this.pets = pets;
 	}
-	
-	
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+
+
+
 
 }
